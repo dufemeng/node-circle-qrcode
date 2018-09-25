7872e0b01c554c08a9db6c80e2d30e81ab753f32
dufemeng <2314627598@qq.com>
2018-09-26 00:41:14 +0800

    some basic

--- examples/server.js
+++ examples/server.js
@@ -4,30 +4,7 @@ var connect = require('express')
 function testQRCode (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/html' })
 
-  // var jungleBook = "The moonlight was blocked out of the mouth of the cave, for Shere Khan's\n" +
-  // 'great square head and shoulders were thrust into the entrance. Tabaqui,\n' +
-  // 'behind him, was squeaking: "My lord, my lord, it went in here!"\n' +
-  // '\n' +
-  // '"Shere Khan does us great honor," said Father Wolf, but his eyes were\n' +
-  // 'very angry. "What does Shere Khan need?"\n' +
-  // '\n' +
-  // "\"My quarry. A man's cub went this way,\" said Shere Khan. \"Its parents\n" +
-  // 'have run off. Give it to me."\n' +
-  // '\n' +
-  // "Shere Khan had jumped at a woodcutter's campfire, as Father Wolf had\n" +
-  // 'said, and was furious from the pain of his burned feet. But Father Wolf\n' +
-  // 'knew that the mouth of the cave was too narrow for a tiger to come in\n' +
-  // "by. Even where he was, Shere Khan's shoulders and forepaws were cramped\n" +
-  // "for want of room, as a man's would be if he tried to fight in a barrel.\n" +
-  // '\n' +
-  // '"The Wolves are a free people," said Father Wolf. "They take orders from\n' +
-  // "the Head of the Pack, and not from any striped cattle-killer. The man's\n" +
-  // 'cub is ours--to kill if we choose."'
-
-
-  var jungleBook = 'https://ycg.qq.com/career/teams'
-
-  // QRCode.QRCodeDraw.color.dark = '#d4d4d4';
+  var lyrics = "在有生的瞬间能遇到你 竟花光所有运气"
 
   var options = {
     scale: 10,
@@ -36,8 +13,7 @@ function testQRCode (req, res) {
     eyesType : 'round'
   }
 
-  QRCode.toDataURL(jungleBook, options, function (err, url) {
-    // console.log(url)
+  QRCode.toDataURL(lyrics, options, function (err, url) {
     if (err) console.log('error: ' + err)
     res.end("<!DOCTYPE html/><html><head><title>node-qrcode</title></head><body><img src='" + url + "'/></body></html>")
   })
